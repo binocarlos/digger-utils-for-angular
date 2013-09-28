86082f49bcbe0ef715748f030bfba17d647fadf0
Kai Davenport <kaiyadavenport@gmail.com>
2013-09-28 19:00:18 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+
+  tools used across the other files
+  
+*/
+
+angular
+  .module('digger.utils', [
+    
+  ])
+
+  .factory('$safeApply', [function($rootScope) {
+    return function($scope, fn) {
+      var phase = $scope.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        if (fn) {
+          $scope.$eval(fn);
+        }
+      } else {
+        if (fn) {
+          $scope.$apply(fn);
+        } else {
+          $scope.$apply();
+        }
+      }
+    }
+  }])
