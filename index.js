b07d6ef4ea301b9d63a9d431a7f8c442b0e3d6be
Kai Davenport <kaiyadavenport@gmail.com>
2013-10-15 09:22:50 +0000

    $containerTreeData service which keeps a static copy of nested tree based on the selector you provide - this is used to render stateful trees that keep their expanded settings
    $getAncestors factory fn that takes a root and a container and returns an array of ancestors for the container up to the root (if found)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 angular
   .module('digger.utils', [
-    
+    'digger.radio'
   ])
 
   .factory('$safeApply', [function($rootScope) {
@@ -25,3 +25,66 @@ angular
       }
     }
   }])
+
+
+  /*
+  
+    load the folders from the resources tree once (but setup a radio)
+    
+  */
+  .service('$containerTreeData', function($q, $rootScope, $containerRadio){
+    var containers = {};
+
+    function load($scope, selector){
+      if(!containers[selector]){
+        var deferred = $q.defer();
+
+        $rootScope.warehouse(selector + ':tree(folder)').ship(function(root){
+          $containerRadio($scope, root);
+          deferred.resolve(root);
+        })
+
+        containers[selector] = deferred.promise;
+      }
+
+      return containers[selector];
+    }
+    
+    return {
+      load:load
+    }
+  })
+
+  /*
+  
+    given a tree root and a container id - return an array of containers
+    that are the ancestors for the id up to the tree root
+    (if the id is found)
+    
+  */
+  .factory('$getAncestors', function(){
+    return function(root, container){
+      var match = root.find('=' + container.diggerid());
+
+      if(match.isEmpty()){
+        return [];
+      }
+
+      var ancestors = [];
+      var current = match;
+
+      while(current){
+        var parent = root.find('=' + current.diggerparentid());
+
+        if(parent.isEmpty()){
+          current = null;
+        }
+        else{
+          ancestors.unshift(parent);
+          current = parent;
+        }
+      }
+
+      return ancestors;
+    }
+  })
